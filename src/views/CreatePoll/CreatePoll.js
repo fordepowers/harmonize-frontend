91b2476a32703d3e2decf81df10d00ac1da9ae32
Valentin Molina <valentinmolia83@gmail.com>
2020-10-17 20:30:11 -0700

    Finish markup for create poll

--- src/views/CreatePoll/CreatePoll.js
+++ src/views/CreatePoll/CreatePoll.js
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CreatePoll.css'
 
+const TIMES = [
+    
+]
+
 export default () => {
+    const [isAllDay, setAllDay] = useState(false);
+    const [title, setTitle] = useState("");
+    const [date, setDate] = useState("");
+
     return (
         <div className="CreatePoll">
             <main className="container">
                 <h1 className="page-heading">Create Poll</h1>
 
                 <label htmlFor="title" className="input-label">Title:</label>
-                <input id="title" placeholder="Enter title" className="input title"></input>
+                <input id="title" onChange={e => setTitle(e.target.value)} value={title} placeholder="Enter title" className="input title"></input>
 
                 <label htmlFor="date" className="input-label">Date:</label>
                 <div className="date-input-container">
-                    <input id="date" type="date" className="input"></input>
+                    <input value={date} onChange={e => setDate(e.target.value)} id="date" type="date" className="input"></input>
                     <div className="checkbox-container">
                         <label className="all-day-label">All Day?</label>
-                        <input id="allDay" type="checkbox"></input>
+                        <input onChange={(e) => setAllDay(e.target.checked)} checked={isAllDay} id="allDay" type="checkbox"></input>
                     </div>
                 </div>
                 
+                {
+                    !isAllDay ? (
+                        <>
+
+                            <label className="input-label">Time Range:</label>
+                            <div className="time-range-input-container">
+                                <div className="time-range-input-wrapper">
+                                    <label className="range-label">From:</label>
+                                    <select className="">
+                                        <option>12:00AM</option>
+                                    </select>
+                                </div>
+
+                                <div className="time-range-input-wrapper">
+                                    <label className="range-label">To:</label>
+                                    <select className="">
+                                        <option>12:00AM</option>
+                                    </select>
+                                </div>
+                              
+                            </div>
+                        </>
+                    ) : null
+                }
                 <button className="create-btn">Create</button>
             </main>
         </div>
